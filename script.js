9976ae9eed8a9682230e92818361eb519e1558c3
JaukG9 <166880340+JaukG9@users.noreply.github.com>
2026-03-06 10:59:14 -0600

    removed SaveData()

--- script.js
+++ script.js
@@ -43,8 +43,6 @@ window.addEventListener("load", function(){
     let L7BP = 0;
     let totalButtonPresses = 0;
 
-    saveData();
-
     if(condition === 1){code = "nb"; implicitness = "low"; adaptability = "no"}
     else if(condition === 2){code = "mb"; implicitness = "moderate"; adaptability = "no"}
     else if(condition === 3){code = "ib"; implicitness = "high"; adaptability = "no"}
@@ -1363,3 +1361,4 @@ window.addEventListener("load", function(){
     animate(0);
 
 });
+
