e51feaa09f5e1510909d0e415aa87af5b23f0fba
JaukG9 <166880340+JaukG9@users.noreply.github.com>
2026-03-06 11:03:40 -0600

    fix?

--- script.js
+++ script.js
@@ -43,6 +43,8 @@ window.addEventListener("load", function(){
     let L7BP = 0;
     let totalButtonPresses = 0;
 
+    saveData();
+    
     if(condition === 1){code = "nb"; implicitness = "low"; adaptability = "no"}
     else if(condition === 2){code = "mb"; implicitness = "moderate"; adaptability = "no"}
     else if(condition === 3){code = "ib"; implicitness = "high"; adaptability = "no"}
@@ -1361,4 +1363,5 @@ window.addEventListener("load", function(){
     animate(0);
 
 });
+
 
